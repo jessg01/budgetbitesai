287f09366d999a21e26f1dffd30c7c2719e9ed7d
Jessi <jessigutam@gmail.com>
2025-03-29 18:20:36 -0500

    new commit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const app = express();
 const userRoute = require('./routes/user.route');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const pythonScriptPath = './budgetbite_ai2.py';
+const { exec } = require('child_process');
+const path = require('path');
 
 app.use(express.json());
 app.use(cors());
@@ -16,3 +19,24 @@ mongoose.connect("mongodb+srv://admin:admin@one.apo14.mongodb.net/BudgetBites?re
 
 //Connect to port
 app.listen(5000, console.log("successfully connected to port"));
+
+
+app.post('/dashboard', (req, res) => {
+    // Specify the path to your Python script
+    const pythonScriptPath = path.join(__dirname, './budgetbite_ai2.py');
+    
+    // Call the Python script using child_process.exec
+    exec(`python3 ${pythonScriptPath}`, (err, stdout, stderr) => {
+      if (err) {
+        console.error('Error executing Python script:', err);
+        return res.status(500).json({ error: 'Python script execution failed' });
+      }
+      if (stderr) {
+        console.error('stderr:', stderr);
+        return res.status(500).json({ error: 'Python script produced an error' });
+      }
+  
+      // Send the output from the Python script back to the client
+      res.json({ result: stdout });
+    });
+  });
